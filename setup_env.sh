ce402dc8dcc562ed794d57b94c5d71943aef5218
utlark <utlark.ya>
2022-08-01 19:20:45 +0500

    Added a mechanism for changing the buildroot version depending on the board.

--- setup_env.sh
+++ setup_env.sh
@@ -1,6 +1,7 @@
 BR2_EXTERNAL="sautExternal"
 BR2_DL_DIR="$BR2_EXTERNAL/downloads"
 TARGET=""
+BR_V=""
 
 function set_target() {
     if [ $# -ne 1 ]; then
@@ -19,6 +20,47 @@ function get_target() {
     echo "Target: $TARGET"
 }
 
+function set_br_version() {
+    if [ $# -ne 1 ]; then
+        echo "Usage: set_br_version version"
+        return
+    fi
+
+    if [ -z $TARGET ]; then
+        echo "Error: need to set target"
+        set_target
+        return
+    fi
+
+    if [ -z $PWD/$BR2_EXTERNAL/board/$TARGET/ ]; then
+        mkdir "$PWD/$BR2_EXTERNAL/board/$TARGET/"
+    fi
+
+    echo $1 > "$PWD/$BR2_EXTERNAL/board/$TARGET/br-version"
+    echo "Set version: $1"
+}
+
+function find_br_version() {
+    if [ -z $TARGET ]; then
+        echo "Error: need to set target"
+        set_target
+        return
+    fi
+
+    if [ -z $PWD/$BR2_EXTERNAL/board/$TARGET/br-version ]; then
+        echo "Error: need to set version"
+        set_br_version
+        return
+    fi
+
+    BR_V="buildroot-$(cat $PWD/$BR2_EXTERNAL/board/$TARGET/br-version)"
+    echo "Find version: $BR_V"
+    return
+
+    echo "Error: could not find buildroot version"
+    return 1
+}
+
 function m() {
     if [ -z $TARGET ]; then
         echo "Error: need to set target"
@@ -26,11 +68,13 @@ function m() {
         return
     fi
 
+    find_br_version
+
     make \
         BR2_EXTERNAL="$PWD/$BR2_EXTERNAL" \
         BR2_DL_DIR="$PWD/$BR2_DL_DIR" \
         O="$PWD/$BULDROOT_OUTPUT" \
-        -C "$PWD/buildroot-2022.02.4" \
+        -C "$PWD/$BR_V" \
         -j$(nproc) \
         "$@"
 }
