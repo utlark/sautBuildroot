7569d691dd4a9a28d5d6256b504701b24dcbc5b1
Yuriy <utlark@ya.ru>
2023-01-12 23:20:59 +0500

    Removed the link to the buildroot version

--- setup_env.sh
+++ setup_env.sh
@@ -1,7 +1,6 @@
 BR2_EXTERNAL="sautExternal"
 BR2_DL_DIR="$BR2_EXTERNAL/downloads"
 TARGET=""
-BR_V=""
 
 function set_target() {
     if [ $# -ne 1 ]; then
@@ -20,47 +19,6 @@ function get_target() {
     echo "Target: $TARGET"
 }
 
-function set_br_version() {
-    if [ $# -ne 1 ]; then
-        echo "Usage: set_br_version version"
-        return
-    fi
-
-    if [ -z $TARGET ]; then
-        echo "Error: need to set target"
-        set_target
-        return
-    fi
-
-    if [ -z $PWD/$BR2_EXTERNAL/board/$TARGET/ ]; then
-        mkdir "$PWD/$BR2_EXTERNAL/board/$TARGET/"
-    fi
-
-    echo $1 > "$PWD/$BR2_EXTERNAL/board/$TARGET/br-version"
-    echo "Set version: $1"
-}
-
-function find_br_version() {
-    if [ -z $TARGET ]; then
-        echo "Error: need to set target"
-        set_target
-        return
-    fi
-
-    if [ -z $PWD/$BR2_EXTERNAL/board/$TARGET/br-version ]; then
-        echo "Error: need to set version"
-        set_br_version
-        return
-    fi
-
-    BR_V="buildroot-$(cat $PWD/$BR2_EXTERNAL/board/$TARGET/br-version)"
-    echo "Find version: $BR_V"
-    return
-
-    echo "Error: could not find buildroot version"
-    return 1
-}
-
 function m() {
     if [ -z $TARGET ]; then
         echo "Error: need to set target"
@@ -68,13 +26,11 @@ function m() {
         return
     fi
 
-    find_br_version
-
     make \
         BR2_EXTERNAL="$PWD/$BR2_EXTERNAL" \
         BR2_DL_DIR="$PWD/$BR2_DL_DIR" \
         O="$PWD/$BULDROOT_OUTPUT" \
-        -C "$PWD/$BR_V" \
+        -C "$PWD/buildroot-2022.02.4" \
         -j$(nproc) \
         "$@"
 }
